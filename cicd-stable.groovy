38d37afb43a537cd28712dd46b7b0c41e49cd3bd
Igor Todorovski <itodorov@ca.ibm.com>
2023-11-28 15:07:56 -0500

    Add cicd scripts

---
+++ cicd-stable.groovy
@@ -0,0 +1,18 @@
+
+node('linux')
+{
+  stage ('Poll') {
+                // Poll for local changes
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/lessport.git']]])
+  }
+
+  stage('Build') {
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/lessport.git'), string(name: 'PORT_DESCRIPTION', value: 'The less terminal pager program.' ), string(name: 'BUILD_LINE', value: 'STABLE')]
+  }
+}
+
