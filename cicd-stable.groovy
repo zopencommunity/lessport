688562c1a081ff3795de97a248ab6266e9f1f7fd
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:35:58 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -8,11 +8,11 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/lessport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/lessport.git']]])
   }
 
   stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/lessport.git'), string(name: 'PORT_DESCRIPTION', value: 'The less terminal pager program.' ), string(name: 'BUILD_LINE', value: 'STABLE')]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/lessport.git'), string(name: 'PORT_DESCRIPTION', value: 'The less terminal pager program.' ), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
 
